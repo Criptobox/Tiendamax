752a3f0be60ef99382dd8327324b205c922022b5
Criptobox <136744712+Criptobox@users.noreply.github.com>
2026-06-11 14:46:11 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -100,7 +100,7 @@
 //      usan el mismo helper _mensajeOrdenWA con formato premium.
 // ═══════════════════════════════════════════════════════
 
-const CACHE_NAME = 'tiendamax-202606111845';
+const CACHE_NAME = 'tiendamax-202606111836';
 
 const STATIC_ASSETS = [
   '/',
